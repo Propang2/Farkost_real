2ae7a36e42cadf1e5c1e3cf8f0c7e14063f566eb
Alexander Strandberg <alexander.strandberg@telia.com>
2019-04-25 17:01:18 +0200

    update

--- EnemyFollowing/PowerUp.cs
+++ EnemyFollowing/PowerUp.cs
@@ -13,12 +13,15 @@ namespace Farkost
         public Vector2 position;
         public Vector2 speed;
         public Texture2D texture;
+
+        //Fixa så den inte är static (eller Rensa bort powerups från lsitan)
         public static int timer;
 
         public PowerUp(Texture2D _texture)
         {
             texture = _texture;
             speed = new Vector2(0, 2);
+
         }
 
         public void Update(GameTime gameTime)
